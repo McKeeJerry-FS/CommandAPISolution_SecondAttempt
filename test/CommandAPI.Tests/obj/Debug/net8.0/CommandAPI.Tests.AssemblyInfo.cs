59ff8eece0b533822de0456e8d20df3f81e7d487
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-06-13 13:02:56 -0400

    updates

--- test/CommandAPI.Tests/obj/Debug/net8.0/CommandAPI.Tests.AssemblyInfo.cs
+++ test/CommandAPI.Tests/obj/Debug/net8.0/CommandAPI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d26f59fa244264d546e58315df63bcb3abd6c694")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
